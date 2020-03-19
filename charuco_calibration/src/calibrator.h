2f063eea4bba0338dab976824a74001b709bec0a
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-03-19 11:19:14 +0300

    calibrator: Use proper order for width/height

--- charuco_calibration/src/calibrator.h
+++ charuco_calibration/src/calibrator.h
@@ -135,7 +135,7 @@ public:
     /** Get cv::Mat with the board image */
     cv::Mat getBoardImage(int width, int height, int margin=0)
     {
-        cv::Mat boardImg(width, height, CV_8UC1);
+        cv::Mat boardImg(height, width, CV_8UC1);
         charucoBoard->draw(boardImg.size(), boardImg, margin);
         return boardImg;
     }
